5d0c7a342d9659e1479e3550b748a25f394de80e
S <s>
2015-07-16 17:25:16 +0530

    {"type":"TI", "runs":"0"}

--- Breakout.java
+++ Breakout.java
@@ -124,7 +124,9 @@ public class Breakout extends GraphicsProgram {
 	
 	
 	private void createBrickRow(int x, int y, Color colorOfRow) {
-		
+		for (int i=0; i<NBRICKS_PER_ROW; i++) {
+			
+		}
 	}
 
 	
