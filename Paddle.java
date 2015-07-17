1b39dd663375c72502c36666bc7d2c15ecdd9250
S <s>
2015-07-17 09:03:38 +0530

    {"type":"TI", "runs":"0"}

--- Paddle.java
+++ Paddle.java
@@ -6,6 +6,10 @@ public class Paddle extends GraphicsProgram {
  *  The paddle object which is used in the Breakout game to bounce the ball back towards the
  *  brick rows.	
  */
+/** Width and height of application window in pixels */
+	public static final int APPLICATION_WIDTH = 400;
+	public static final int APPLICATION_HEIGHT = 600;	
+	
 /** Dimensions of game board (usually the same) */
 	private static final int WIDTH = APPLICATION_WIDTH;
 	private static final int HEIGHT = APPLICATION_HEIGHT;
