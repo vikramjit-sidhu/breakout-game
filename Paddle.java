59109efdb243da075bc8a3bf0eaae5d26cfa5b0d
S <s>
2015-07-17 09:13:00 +0530

    {"type":"TI", "runs":"0"}

--- Paddle.java
+++ Paddle.java
@@ -3,17 +3,8 @@ import acm.program.GraphicsProgram;
 /**
  *  The paddle object which is used in the Breakout game to bounce the ball back towards the
  *  brick rows.	
+ *  Simply creates a GRect object with the desired attributes, sets mouse move event also for the paddle
  */
 public class Paddle extends GraphicsProgram {
-/** Width and height of application window in pixels */
-	public static final int APPLICATION_WIDTH = 400;
-	public static final int APPLICATION_HEIGHT = 600;	
 	
-/** Dimensions of game board (usually the same) */
-	private static final int WIDTH = APPLICATION_WIDTH;
-	private static final int HEIGHT = APPLICATION_HEIGHT;
-	
-	public void run() {
-		drawPaddle();
-	}
 }
