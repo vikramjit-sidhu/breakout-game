76092f6369ba3b0b2b32d7aa9cef8921afa66d9e
S <s>
2015-07-20 10:16:18 +0530

    {"type":"TI", "runs":"0"}

--- Breakout.java
+++ Breakout.java
@@ -70,7 +70,7 @@ public class Breakout extends GraphicsProgram {
 	
 	
 /**	
- * Creates the row of bricks near top of window
+ * 	Creates the row of bricks near top of window
  * 	the rows are coloured in groups of 2.
  */
 	private void setupBricks() {
