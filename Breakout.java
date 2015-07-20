be277db225981b7f6c4a061b09889410e9abd993
S <s>
2015-07-20 14:55:55 +0530

    {"type":"TI", "runs":"0"}

--- Breakout.java
+++ Breakout.java
@@ -162,6 +162,7 @@ public class Breakout extends GraphicsProgram {
 		else if ((distanceToMove > 0) && ((paddle.getX() + PADDLE_WIDTH + distanceToMove) <= WIDTH)) {
 			return false;
 		}
+		return true;
 	}
 	
 	
