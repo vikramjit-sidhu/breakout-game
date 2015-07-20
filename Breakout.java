24e54fe7233cdbb593b499e004f19ed2ca5fc1b9
S <s>
2015-07-20 11:53:07 +0530

    {"type":"TI", "runs":"0"}

--- Breakout.java
+++ Breakout.java
@@ -87,7 +87,8 @@ public class Breakout extends GraphicsProgram {
  *  Adds color to the paddle, which is the defined constant
  */
 	private void addPaddle() {
-		/* The x location of the paddle, so that it will be at centre of screen, minus half the 
+		/* The x location of the paddle, i.e. the GRect object which is at the x, y location of the screen. 
+		 * So that it will be at centre of screen, minus half the 
 		 * paddle width. */
 		int paddleXLocation = (getWidth() / 2) - (PADDLE_WIDTH / 2);
 		/* The y offset of the paddle is given from the bottom of the screen, 
