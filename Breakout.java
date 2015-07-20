2d5f58a74d8bc36456deec20338380b544d80198
S <s>
2015-07-20 15:42:35 +0530

    {"type":"TI", "runs":"0"}

--- Breakout.java
+++ Breakout.java
@@ -140,7 +140,7 @@ public class Breakout extends GraphicsProgram {
 		if (!checkPaddleCollision(distanceToMove)) {
 			Point oldLocation = new Point();
 			oldLocation.setLocation(paddle.getX(), paddle.getY());
-			paddle.move(oldLocation.getX() + distanceToMove, 0);
+			paddle.move(distanceToMove, 0);
 		}
 	}
 	
