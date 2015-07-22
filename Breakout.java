f283636a9d74756ca33c58cc904812199341a26b
S <s>
2015-07-22 17:06:21 +0530

    {"type":"TI", "runs":"2"}

--- Breakout.java
+++ Breakout.java
@@ -109,8 +109,8 @@ public class Breakout extends GraphicsProgram {
  * 	co-ordinates are the top left corner of the rectangle and not the center of ball	
  */
 	private void setBallAtInitialLocation() {
-		double ballx = getWidth() - (BALL_RADIUS / 2);
-		double bally = getHeight() - (BALL_RADIUS / 2);
+		double ballx = (getWidth() / 2) - (BALL_RADIUS / 2);
+		double bally = (getHeight() / 2) - (BALL_RADIUS / 2);
 		ball = new GOval(ballx, bally, BALL_RADIUS, BALL_RADIUS);
 		ball.setFilled(true);
 		ball.setFillColor(BALL_COLOR);
