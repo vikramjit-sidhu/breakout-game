eed2855737dc8c9e34b807677fa98889448ee960
S <s>
2015-07-23 11:42:46 +0530

    {"type":"TI", "runs":"1"}

--- Breakout.java
+++ Breakout.java
@@ -104,6 +104,7 @@ public class Breakout extends GraphicsProgram {
 	private void startGame() {
 		for (int i=0; i < NTURNS; i++) {
 			initializeInitialVelocityOfBall();
+			moveBall();
 		}
 	}
 	
