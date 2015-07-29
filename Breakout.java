66776025e6f7db5bacca6bac8ee8211590c2304a
S <s>
2015-07-29 16:32:08 +0530

    {"type":"TI", "runs":"0"}

--- Breakout.java
+++ Breakout.java
@@ -185,7 +185,7 @@ public class Breakout extends GraphicsProgram {
 		while (true) {
 			moveBall();
 			handleBallCollisions();
-			if (checkForTurnOver()) {
+			if (checkIfTurnOver()) {
 				break;
 			}
 			pause(DELAY);
