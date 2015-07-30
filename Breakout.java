d5a6b6dd08ff1b9e5486b6263224d4fbb8fd3a0d
S <s>
2015-07-30 10:49:15 +0530

    {"type":"TI", "runs":"0"}

--- Breakout.java
+++ Breakout.java
@@ -260,7 +260,7 @@ public class Breakout extends GraphicsProgram {
 			invertYVelocity();
 			/* Removing brick from screen, 
 			 * and reducing the number of bricks left (instance variable numBricksRemInTurn */
-			
+			remove(objectAtBallLocation);
 		}
 	}
 	
