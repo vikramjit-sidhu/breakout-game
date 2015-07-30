f4936fdd6975fe1951835e7c31d8c9b4215421e8
S <s>
2015-07-30 11:25:20 +0530

    {"type":"TI", "runs":"0"}

--- Breakout.java
+++ Breakout.java
@@ -284,6 +284,7 @@ public class Breakout extends GraphicsProgram {
 		double ballWidth = 2 * BALL_RADIUS;
 		/* Getting object at top left corner of square */
 		collidingObject = getElementAt(ballxPos, ballyPos);
+		/* Getting object at top right corner of square */
 		if (collidingObject == null) {
 			collidingObject = getElementAt(ballxPos + ballWidth, ballyPos);
 		}
