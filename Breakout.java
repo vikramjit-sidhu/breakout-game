e50d8a235a16728e303d67367bde8ea66427e5b2
S <s>
2015-07-30 13:53:46 +0530

    {"type":"TI", "runs":"0"}

--- Breakout.java
+++ Breakout.java
@@ -53,7 +53,8 @@ public class Breakout extends GraphicsProgram {
 /** Radius of the ball in pixels */
 	private static final int BALL_RADIUS = 10;
 	
-/**	The width of the square which contains the ball object */	
+/**	The width of the square which contains the ball object */
+	private static final int BALL_WIDTH = BALL_RADIUS * 2;
 	
 /**	Color of the ball */
 	private static final Color BALL_COLOR = Color.BLACK;
