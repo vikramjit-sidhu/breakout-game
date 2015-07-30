4f71a9a0f7df745d45d104453a63f57f68d88c9f
S <s>
2015-07-30 14:02:29 +0530

    {"type":"TI", "runs":"0"}

--- Breakout.java
+++ Breakout.java
@@ -308,7 +308,7 @@ public class Breakout extends GraphicsProgram {
 		collidingObject = getElementAt(ballTopLeftX, ballTopLeftY);
 		/* Since no object found yet, finding if there is an object at top right corner of square */
 		if (collidingObject == null) {
-			collidingObject = getElementAt(ballxPos + BALL_WIDTH, ballyPos);
+			collidingObject = getElementAt(ballTopLeftX + BALL_WIDTH, ballTopLeftY);
 		}
 		/* Since no object found yet, finding if there is an object at bottom left corner of square */
 		if (collidingObject == null) {
